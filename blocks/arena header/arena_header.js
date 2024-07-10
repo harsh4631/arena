58f9b59f4a6e6727f325ca11cbde1487fc914d64
Harsh Jain <hj9049830@gmail.com>
2024-07-10 12:18:05 +0530

    update

--- blocks/arena header/arena_header.js
+++ blocks/arena header/arena_header.js
@@ -1,6 +1,30 @@
-// export default function decorateHeader(block){
-//     function getHeaderData(block){
-//         const [back,front] = block.children;
-//         const 
-//     }
-// }
+import utility from '../../utility/utility.js';
+export default function decorate(block){
+    function getArena_HeaderData(block){
+    const[
+        logo,
+        listtitle
+    ]= block.children;
+
+    const logotitle=logo?.textContent.trim();
+    const list = Array.from(listtitle?[listtitle]:[]).map((p) => p.outerHTML).join('');
+  
+    return {
+        logotitle,
+        list
+    };
+}
+
+
+const arenaData = getArena_HeaderData(block);
+
+const areanaHtml = utility.sanitizeHtml(`
+    <div class="arena_header">
+    ${arenaData.logotitle ? `<h1>${arenaData.logotitle}</h1>` : ''}
+    <div class = "arena_list">
+    ${arenaData.list ? `${arenaData.list}` : ''}
+    </div>
+    </div>
+    `);
+    block.innerHTML = areanaHtml;
+} 
