926de26696d0921ab17e55cacff55b2a546b9a29
Harsh Jain <hj9049830@gmail.com>
2024-07-10 16:33:00 +0530

    update

--- blocks/arena-header/arena-header.js
+++ blocks/arena-header/arena-header.js
@@ -1,19 +1,26 @@
 // import utility from '../../utility/utility.js';
 export default function decorate(block){
     function getArena_HeaderData(block){
-    const[
-        logo,
-        listtitle
-    ]= block.children;
+    const[logo, listtitle] = block.children;
 
-    const logotitle=logo?.textContent.trim();
-    const list=listtitle?.textContent.trim();
-    // const list = Array.from(listtitle?[listtitle]:[]).map((p) => p.outerHTML).join('');
-       
-    // console.log("my value is" ,logotitle);
+     const logotitle = logo?.textContent.trim();
+    //const listName = Array.from(listtitle?[listtitle]:[]).map((p) => p.outerHTML).join('');
+
+    console.log("my current title value is+++++",logotitle);
+    // console.log("my new valu is *****************",listName);
+
+
+    //const logotitle=logo?.querySelector('p').textContent.trim();
+    // const listName=listtitle?.textContent.trim();
+    // const sublist = listtitle.querySelectorAll('p');
+    // console.log("hello",sublist);
+
+
+
+    // console.log("my name",listName);
     return {
-        logotitle,
-        list
+        logotitle
+        //listName
     };
     
 }
@@ -21,15 +28,13 @@ export default function decorate(block){
 
 const arenaData = getArena_HeaderData(block);
     // console.log(arenaData.logotitle);
-    // console.log(arenaData.list);
+    // console.log(arenaData.listName);
 const areanaHtml = `
     <div class="arena_header">
     ${arenaData.logotitle ? `<h1 class = "abc">${arenaData.logotitle}</h1>` : ''}
 
-    <div class = "arena_list">
-    ${arenaData.list ?  `<p class="bcd"> ${arenaData.list}</p>` : ''}
+   
     </div>
-    </div>
-    `;
+`;
     block.innerHTML = areanaHtml;
 } 
