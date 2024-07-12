03de4902fd23b8590eb6c6446ccb31168658e62a
Harsh Jain <hj9049830@gmail.com>
2024-07-12 12:02:14 +0530

    update

--- blocks/teasers/teasers.js
+++ blocks/teasers/teasers.js
@@ -1,1 +1,36 @@
-console.log("hello budyy")
+export default function decorate(block){
+    function getteaserData(block){
+
+        // console.log("bye");
+        const[main,subcard]=block.children;
+
+        const maintitle = main?.textContent.trim();
+        const image123 = subcard.querySelector('picture');
+        const pretitle = subcard?.textContent.trim();
+        const description1 = Array.from(subcard?[subcard]:[]).map((p) => p.outerHTML).join('');
+
+        console.log("hello",pretitle);
+        return{
+            maintitle,
+            image123,
+            pretitle,
+            description1
+        };
+    }
+    const teaserData = getteaserData(block);
+    const teasertitle = `
+    <div class="main-teaser">
+       <div class ="main-title">
+       ${teaserData.maintitle ? `<h1>${teaserData.maintitle}</h1>` : ''}
+       </div>
+       
+       <div class="sub-teaser">
+       ${teaserData.image123 ? teaserData.image123.outerHTML : ''}
+       ${teaserData.pretitle ? `<p class="pre">${teaserData.pretitle}</p>` : ''}
+       ${teaserData.description1 ? `<p class = "des">${teaserData.description1}</p>` : ''}
+       </div>
+    </div>
+    `;
+    block.innerHTML = teasertitle;
+}
+
