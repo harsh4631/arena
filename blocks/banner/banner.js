a0a710878f8fb26b5706e3c1541e195e7ff1642b
Harsh Jain <hj9049830@gmail.com>
2024-07-16 17:33:58 +0530

    update

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,1 +1,57 @@
-console.log("he");
+export default function decorate(block){
+    function getbannerData(block){
+        const[bannerimg , bannerpara, bannerdes, descbanner] = block.children;
+
+        const dispimg = bannerimg.querySelector('picture');
+
+        const predisp = bannerpara.querySelector('p');
+
+        const dese1 = bannerdes.querySelector('p');
+
+        const nexadesc = descbanner.querySelector('p');
+
+        // const nexadesc = bannerpara.querySelector('p');
+
+        const prenexa = predisp?.textContent.trim();
+
+        const scrolnexa = Array.from(dese1 ? [dese1]:[]).map ((p) =>p.outerHTML).join('');
+
+        const descnexa = Array.from(nexadesc ? [nexadesc]:[]).map ((p) =>p.outerHTML).join('');
+
+        console.log("bye",scrolnexa);
+
+        return{
+            dispimg,
+            prenexa,
+            scrolnexa,
+            descnexa
+        };
+
+    }
+    const bannernexa = getbannerData(block);
+
+    const htmlbanner = `
+    <div class="banner-main">
+       <div class="banner-img">
+       ${bannernexa.dispimg ? bannernexa.dispimg.outerHTML : ''}
+      </div>
+
+         <div class="banner-sub">
+         ${bannernexa.prenexa ? `<p class = "def">${bannernexa.prenexa}</p>` : ''}
+
+          <div class="banner-descone">
+          ${bannernexa.scrolnexa ? `${bannernexa.scrolnexa}` : ''}
+          </div>
+
+           <div class="banner-destwo">
+          ${bannernexa.descnexa ? `${bannernexa.descnexa}` : ''}
+          </div>
+
+         </div>
+
+
+    </div>
+
+    `;
+    block.innerHTML = htmlbanner;
+}
--- blocks/teasers/teasers.js
+++ blocks/teasers/teasers.js
@@ -1,12 +1,12 @@
 export default function decorate(block){
     function getteasercardData(block){
-        const[toparea,bottomarea]=block.children;
+        const[toparea,bottomarea,desbottom]=block.children;
 
         const mainheading = toparea?.textContent.trim();
         const imague23 = bottomarea.querySelector('picture');
-        // const belowdes = bottomarea.querySelectorAll('p');
+        const belowdes = desbottom.querySelector('p');
         const viewtitle = bottomarea?.textContent.trim();
-        const viewdesc = Array.from(bottomarea?[bottomarea]:[]).map((p) => p.outerHTML).join('');
+        const viewdesc = Array.from(belowdes?[belowdes]:[]).map((p) => p.outerHTML).join('');
 
         console.log("hello",viewdesc);
         return{
@@ -28,14 +28,14 @@ export default function decorate(block){
     <div class="viewsub-teaser">
     
     <div class="viewimg-teaser">
-    ${teascarData ? teascarData.imague23.outerHTML : ''}
+    ${teascarData.imague23 ? teascarData.imague23.outerHTML : ''}
     </div>
     <div class="viewpara-teaser">
-    ${teascarData ? `${teascarData.viewtitle}` : ''}
+    ${teascarData.viewtitle ? `${teascarData.viewtitle}` : ''}
      </div> 
 
      <div class="viewdes-teaser">
-    ${teascarData ? `<p>${teascarData.viewdesc}</p>` : ''}
+    ${teascarData.viewdesc ? `<p>${teascarData.viewdesc}</p>` : ''}
     </div>
     </div>
     </div>
